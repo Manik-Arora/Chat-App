f167f65bb376707be640a852d7979a936b08be01
Manik-Arora <manikarora2605@gmail.com>
2024-08-15 19:54:30 +0530

    Created top, botton for Chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,37 @@
 import "./chat.css";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./bg.jpg" alt="hhh" />
+          <div className="texts">
+            <span>Paul Walker</span>
+            <p>Hey, there I'm using this chat app.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder="Type a message..." />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" />
+        </div>
+        <div className="sendButton">Send</div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
