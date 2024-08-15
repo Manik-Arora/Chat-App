95c5ea3cbe2dffdfae4addad7562614bda886898
Manik-Arora <manikarora2605@gmail.com>
2024-08-15 17:48:17 +0530

    Adding List component

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,7 +1,95 @@
+import { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
-  return <div className="chatList"></div>;
+  const [addMode, setAddMode] = useState(false);
+
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./bg.jpg" alt="" />
+        <div className="texts">
+          <span>Paul Walker</span>
+          <p>Hey, how r u doing?</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatList;
