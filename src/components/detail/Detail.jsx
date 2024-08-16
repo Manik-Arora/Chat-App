2cf724b243dcb08ad96412fdf72c8e07eecc855f
Manik-Arora <manikarora2605@gmail.com>
2024-08-16 18:44:13 +0530

    Details page completed

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,85 @@
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./bg.jpg" alt="" />
+        <h2>Paul Walker</h2>
+        <p>Hey there! I'm using this chat app.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?cs=srgb&dl=pexels-pixabay-302769.jpg&fm=jpg"
+                  alt=""
+                />
+                <span>Photo_2024_1.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?cs=srgb&dl=pexels-pixabay-302769.jpg&fm=jpg"
+                  alt=""
+                />
+                <span>Photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://burst.shopifycdn.com/photos/skyline-at-sunset.jpg?width=1000&format=pjpg&exif=0&iptc=0"
+                  alt=""
+                />
+                <span>Photo_2024_3.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://burst.shopifycdn.com/photos/skyline-at-sunset.jpg?width=1000&format=pjpg&exif=0&iptc=0"
+                  alt=""
+                />
+                <span>Photo_2024_3.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
