06a29dcd8db6ec7c506d36af2a87b40a52e2f182
Manik-Arora <manikarora2605@gmail.com>
2024-08-18 21:37:33 +0530

    Adding login functionality

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,22 +5,16 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.STORAGE_BUCKET,
-  appId: import.meta.env.APP_ID,
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
-export const db = getFirestore();
+export const db = getFirestore(app);
 export const storage = getStorage();
-
-// authDomain: "reactchatapp-fafa7.firebaseapp.com",
-//   projectId: "reactchatapp-fafa7",
-//   storageBucket: "reactchatapp-fafa7.appspot.com",
-//   messagingSenderId: "1055044147542",
-//   appId: "1:1055044147542:web:30e75836af8b8059ed1ef7"
