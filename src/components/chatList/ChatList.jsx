4d4fd0176d2263e5691bd874afa06c1036ea8f9e
Manik-Arora <manikarora2605@gmail.com>
2024-08-21 10:23:53 +0530

    Addinf user search functionality

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/AddUser";
+import { useUserStore } from "../../lib/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../lib/firebase";
 
 const ChatList = () => {
+  const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
+
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(docRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+    return () => unSub();
+  }, [currentUser.id]);
 
   return (
     <div className="chatList">
@@ -19,76 +47,16 @@ const ChatList = () => {
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./bg.jpg" alt="" />
+          <div className="texts">
+            <span>Paul Walker</span>
+            <p>Hey, how r u doing?</p>
+          </div>
         </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./bg.jpg" alt="" />
-        <div className="texts">
-          <span>Paul Walker</span>
-          <p>Hey, how r u doing?</p>
-        </div>
-      </div>
+      ))}
+
       {addMode && <AddUser />}
     </div>
   );
--- src/components/chatList/addUser/AddUser.jsx
+++ src/components/chatList/addUser/AddUser.jsx
@@ -1,19 +1,45 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
 import "./addUser.css";
+import { db } from "../../../lib/firebase";
+import { useState } from "react";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q);
+      console.log(querySnapShot);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form action="">
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="detail">
-          <img src="./bg.jpg" alt="" />
-          <span>Paul Walker</span>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./bg.jpg"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
         </div>
-        <button>Add User</button>
-      </div>
+      )}
     </div>
   );
 };
